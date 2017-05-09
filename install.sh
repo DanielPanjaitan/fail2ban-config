294f3b2b95ed5c3052f147eb751b633d222630bb
Daniel Panjaitan <daniel.pandjaitan1996@gmail.com>
2017-05-09 11:46:13 +0700

    update install

--- install.sh
+++ install.sh
@@ -1,10 +1,28 @@
 #!/bin/bash
 doing_update(){
   echo "=============DOING SYSTEM UPDATE============="
-  apt-get update
+  #apt-get update
 }
 
+install_packages(){
+  echo "=============INSTALLING PACKAGES============="
+  #apt install fail2ban -y
+  #apt install sendmail -y
+}
+
+config_packages(){
+  echo "=============CONFIGURING PACKAGES============="
+   #wget https://raw.githubusercontent.com/DanielPanjaitan/fail2ban-config/master/jail.local -P /etc/fail2ban/
+   #rm -rf /etc/fail2ban/action.d/iptables-multiport.conf
+   #wget https://raw.githubusercontent.com/DanielPanjaitan/fail2ban-config/master/iptables-multiport.conf -P /etc/fail2ban/action.d/
+   #systemctl status fail2ban
+} 
+
 main (){
-shout_something
+  doing_update
+  install_packages
+  config_packages
+  echo "Installation Complete"
+  exit 0
 }
 main
